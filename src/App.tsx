1436eee569457bee00591666d93010aeae8d1591
michael seaman <mseaman26@gmail.com>
2025-01-19 01:28:55 -0800

    .env test

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+const apiUrl = import.meta.env.VITE_API_URL;
 
 function App() {
   const [count, setCount] = useState(0)
+  console.log(apiUrl);
 
   return (
     <>
